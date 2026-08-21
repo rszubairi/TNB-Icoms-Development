72ca42456ec2da29b21ed9fef507f4772f4b7996
khurum.khan <khurum.khan@ebusinesspal.com.my>
2026-08-21 13:08:36 +0800

    fixed the hosting in release version to port 8080

--- src/TnbIcoms.Api/Program.cs
+++ src/TnbIcoms.Api/Program.cs
@@ -46,6 +46,11 @@ using TnbIcoms.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
+#if !DEBUG
+    // Configure to listen on port 8080 for AWS App Runner
+    builder.WebHost.UseUrls("http://+:8080");
+#endif
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
